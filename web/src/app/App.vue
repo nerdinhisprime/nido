d10c7bd674b8e89ed1b7d9a08e1bb36a0c2d3a17
nerdinhisprime <nerdinhisprime@gmail.com>
2026-05-19 17:33:13 +0000

    add and setting typescript

---
+++ web/src/app/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+  import { ref } from 'vue';
+  var variable = ref<string>('something interesting');
+</script>
+<template>
+  <article>
+    <h1>{{ variable }}</h1>
+  </article>
+</template>
---
+++ web/src/app/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import app from './App.vue';
+import './styles/normalize.css';
+
+createApp(app).mount('#app');
